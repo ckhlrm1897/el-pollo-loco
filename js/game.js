0334a93e9acb8e483d3e914b502ad9388df18dea
Christian Klemm <christianklemm1987@t-online.de>
2025-07-29 10:29:24 +0200

    add class movable object, character, chicken and world

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let world = new World();
 
 function init(){
     canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d')
-    character.src = '../img/img/2_character_pepe/2_walk/W-21.png'
-    ctx.drawImage(character, 20, 20, 50, 150);
+    ctx = canvas.getContext('2d');
+
+    console.log('My Character is', world['character']);
     
 }
