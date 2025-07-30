54a5d81edcdf600b39c972f3935bfe6a12af58e5
Christian Klemm <christianklemm1987@gmail.com>
2025-07-30 07:57:07 +0200

    create image cache for character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,24 @@ class MovableObject {
     img;
     height = 230;
     width = 130;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image1.png',...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+
     }
 
     moveRight() {
