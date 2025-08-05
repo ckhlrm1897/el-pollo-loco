6f5b30d5c0011131b8caa1acbecd1152bb407810
Christian Klemm <christianklemm1987@t-online.de>
2025-08-05 21:01:39 +0200

    fix: healthbar equal 80 60 40 or 20 return 0

--- models/bars/health-bar.class.js
+++ models/bars/health-bar.class.js
@@ -34,13 +34,13 @@ class HealthBar extends StatusBar {
     resolveImageIndex() {
         if (this.percentage == 100) {
             return 5;
-        } else if (this.percentage < 100 && this.percentage > 80) {
+        } else if (this.percentage < 100 && this.percentage >= 80) {
             return 4;
-        } else if (this.percentage < 80 && this.percentage > 60) {
+        } else if (this.percentage < 80 && this.percentage >= 60) {
             return 3;
-        } else if (this.percentage < 60 && this.percentage > 40) {
+        } else if (this.percentage < 60 && this.percentage >= 40) {
             return 2;
-        } else if (this.percentage < 40 && this.percentage > 20) {
+        } else if (this.percentage < 40 && this.percentage >= 20) {
             return 1;
         } else {
             return 0;
